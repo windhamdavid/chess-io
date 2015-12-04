8972d6ba95133c018e775c928b0e89bcefd00746
windhamdavid <david@davidawindham.com>
2015-12-04 07:16:02 -0500

    fix favicon

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(favicon(path.join(__dirname, rootDir + '/img/favicon.png')));
+app.use(favicon(path.join(__dirname, rootDir + '/img/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
