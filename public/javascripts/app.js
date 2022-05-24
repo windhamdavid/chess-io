111f585537fb223ccd212d511f8bb621da7245b4
windhamdavid <david@davidawindham.com>
2022-05-24 08:56:55 -0400

    change domain

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -13,8 +13,8 @@ $(function () {
     $URL = 'http://code.davidawindham.com:8888';
     $WS = 'ws://code.davidawindham.com:8888/';
   } else if (ENV === 'chess') {
-    $URL = 'http://chess.davidawindham.com';
-    $WS = 'ws://chess.davidawindham.com:8181/';
+    $URL = 'http://chess.davidwindham.com';
+    $WS = 'ws://chess.davidwindham.com:8181/';
   }
 
   $socket = io.connect($WS);
